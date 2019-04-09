a8daf7c2f3a8fd6035e6c0b841190b05b81d5989
Nathan Bland <nathan.bland@gmail.com>
2019-04-09 13:35:38 -0600

    Updates

--- sw.js
+++ sw.js
@@ -1,40 +1,48 @@
-importScripts('/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/framework/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/33ddba5d5abb1294718c.js",
-    "revision": "fe71ba6fb12061cc7d3c509a0ba826b6"
+    "url": "/framework/06167500c82ff79d39f0.js",
+    "revision": "6356930d919de262666a8467726fc8f4"
   },
   {
-    "url": "/_nuxt/3f7f69240c30882899b8.js",
-    "revision": "4374e8304d786f2908309fa95ba60ada"
+    "url": "/framework/194b72fcd0e10d8301b3.js",
+    "revision": "26a9639030ae2111a041bd29143b64a8"
   },
   {
-    "url": "/_nuxt/4303575a1bb08a41ffd7.js",
-    "revision": "73864beea6b31b71d205ba77b576a2cc"
+    "url": "/framework/3c9bc6c154b2bad5ea52.js",
+    "revision": "4ef0e67eef7a7bc498fd4c0542509223"
   },
   {
-    "url": "/_nuxt/704523aac619bfd5daa6.js",
-    "revision": "9a00b3f15a4e5262c3666c1d444cb40a"
+    "url": "/framework/575c874379c1fe58ea79.js",
+    "revision": "1990eed94e095cabe0e5c48e3e468f3f"
   },
   {
-    "url": "/_nuxt/84b04392db3ab1ec6ef0.js",
-    "revision": "ab1392a31dd5e4b2914a4a4ad29c0226"
+    "url": "/framework/8042860e54bd17c83286.js",
+    "revision": "6dad6590f651ef7f9fb111abb2cd97eb"
   },
   {
-    "url": "/_nuxt/90262f240d1b2f302a15.js",
-    "revision": "73d61e97c0e7203d85e4642b7783d1c5"
+    "url": "/framework/950bada5b39f02787ed4.js",
+    "revision": "2f42b53814db369184ae1d31dca2fee8"
   },
   {
-    "url": "/_nuxt/b46fd3379fd3ae3e238b.js",
-    "revision": "0530c20334caf729ba64a834317cb94a"
+    "url": "/framework/b4a5cb60cffa2ca46899.js",
+    "revision": "2b197d10d3986bd09adb6ed84cffdb32"
   },
   {
-    "url": "/_nuxt/eee9634f20559caeb485.js",
-    "revision": "2b63548494ed2db1383b1144f49a9881"
+    "url": "/framework/cba277142249cd9acc00.js",
+    "revision": "ba2b6ae315675b8c76ea7be29763d773"
+  },
+  {
+    "url": "/framework/f941c16bb1cfa212ccb6.js",
+    "revision": "20e864793607e7c8147631db4526409a"
+  },
+  {
+    "url": "/framework/ff79a8c278a4b8c9e621.js",
+    "revision": "2bfa52eb2fb554a60dd9483eb9c7ca87"
   }
 ], {
-  "cacheId": "NathanBland.github.io",
+  "cacheId": "nathanbland.dev",
   "directoryIndex": "/",
   "cleanUrls": false
 })
@@ -42,6 +50,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/framework/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
 workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')

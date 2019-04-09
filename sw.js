fda1ec87d02994f02fd1c33c1f0879b9b82cb3ff
Nathan Bland <nathan.bland@gmail.com>
2019-04-09 11:14:10 -0600

    Updates

--- sw.js
+++ sw.js
@@ -2,40 +2,44 @@ importScripts('/app/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/app/20f442e9a71bbe859b0f.js",
-    "revision": "b720a4e102ce47965576d13e201e91ec"
+    "url": "/app/141253c2aeff438fe2f6.js",
+    "revision": "e42012aa75207f5c4d9f61314af25e8a"
   },
   {
-    "url": "/app/2dd697d5a0bb80616527.js",
-    "revision": "f47224aba9ce89b54b952c4048cfb80f"
+    "url": "/app/2bf79c4847c6e681b950.js",
+    "revision": "67e31c87f3dc47fa1a78fff4116020f3"
   },
   {
-    "url": "/app/5f2e446d3e30be25a815.js",
-    "revision": "8175639ac9499953d3512d464ef1082b"
+    "url": "/app/2ff0bdd6bcd84fb5ef44.js",
+    "revision": "b9e17b52baef38e600f42c1635af8908"
   },
   {
-    "url": "/app/85d589556c89e4bc6e75.js",
-    "revision": "aa1585d420391fa37c04a611674e102c"
+    "url": "/app/36d444b2aea95af0b32b.js",
+    "revision": "703f8d3e0d50a7b0ff2fdfa63bfd0f2d"
   },
   {
-    "url": "/app/881512400e767a191d08.js",
-    "revision": "b810d39d029ef807d63da8762351a33f"
+    "url": "/app/3bb043d6186af0c3f093.js",
+    "revision": "6e5f1703098164f9a5d0c2140abc9a84"
   },
   {
-    "url": "/app/94ac10d515b3b5ed394d.js",
-    "revision": "b46fc98b18c6ad2d4261a9094622524c"
+    "url": "/app/4493c8f6f6002fc1f668.js",
+    "revision": "7ca2dbb01632ef305b725ff49cea600c"
   },
   {
-    "url": "/app/b4bbf995cf290785f306.js",
-    "revision": "87dcad0aba084a40174d9a3f399d45e6"
+    "url": "/app/6f6b6daaa30bc13bf9cd.js",
+    "revision": "cecdfba3f64971ff8bcdbd5bd4de106f"
   },
   {
-    "url": "/app/bfcd002fe4045d8e680e.js",
-    "revision": "baccb3c626151291dffba739ffec62b1"
+    "url": "/app/7d7a0202df01a964d2a0.js",
+    "revision": "033e0a7872395a1173790eef938a03a8"
   },
   {
-    "url": "/app/db1ab69c0fe6db64edeb.js",
-    "revision": "f29d122470a1b096c0faba4810c109ab"
+    "url": "/app/869c3815f32aedc64042.js",
+    "revision": "a6e561338a3b34274441f5a18af55b34"
+  },
+  {
+    "url": "/app/bd5c746264bb09a88478.js",
+    "revision": "c4aef595a26e612050be4e8c1773f038"
   }
 ], {
   "cacheId": "NathanBland.github.io",

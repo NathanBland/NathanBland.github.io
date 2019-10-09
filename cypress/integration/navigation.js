a81fc8b6fdbc3289c07cfb2ed977f52333d60bca
Nathan Bland <nathan.bland@gmail.com>
2019-10-08 22:57:20 -0600

    Add tests

---
+++ cypress/integration/navigation.js
@@ -0,0 +1,24 @@
+describe('Site Tests', () => {
+  it('Should be able to open the home page', () => {
+    cy.visit('/')
+    cy.contains('NathanBland.dev')
+    cy.contains('Blog')
+    cy.contains('GitHub')
+    cy.contains('Dev.to')
+    cy.contains('Twitter')
+    cy.get('.profile > img')
+      .should('be.visible')
+  })
+  it('Should be able to navigate to the Blog', () => {
+    cy.contains('Blog').click()
+    cy.url().should('contain', '/blog')
+  })
+  it('Should be able to navigate to a blog post', () => {
+    cy.server()
+    cy.route('GET', '**/*.md').as('post')
+    cy.get('.posts-list > a').first().click()
+    cy.wait('@post')
+    cy.get('.post-body')
+      .should('not.be.empty')
+  })
+})

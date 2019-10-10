a55135552310094c4e0eb5cf5f1ec3f1668fb9d1
Nathan Bland <nathan.bland@gmail.com>
2019-10-10 15:06:20 -0600

    Update root title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Nathan Bland | Dev',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
